3d11e642404d89cd7748efac9bbf18efefb870c5
Eduardo Motta de Moraes <edumoraes@live.com>
2021-10-03 18:43:58 -0300

    add create_spend_chart function, get_withdrawal method

--- Scientific Computing with Python/Project - Budget App/budget.py
+++ Scientific Computing with Python/Project - Budget App/budget.py
@@ -19,6 +19,12 @@ class Category:
             balance += i["amount"]
         return balance
 
+    def get_withdrawals(self):
+        withdrawals = 0
+        for i in self.ledger:
+            if i["amount"] < 0: withdrawals += i["amount"]
+        return withdrawals
+
     def transfer(self, amount, other_category):
         if self.check_funds(amount):
             self.withdraw(amount, f"Transfer to {other_category.category}")
@@ -35,7 +41,7 @@ class Category:
 
         items = ''
         for i in self.ledger:
-            items += f'{i["description"]:23.23}{i["amount"]:7.2f}' + "\n"
+            items += f"{i['description']:23.23}{i['amount']:7.2f}" + "\n"
 
         total = f"Total: {self.get_balance():0.2f}"
 
@@ -43,4 +49,40 @@ class Category:
 
 
 def create_spend_chart(categories):
-    pass
+    
+    first_column =  "1           "
+    second_column = "0987654321  "
+    third_column =  "00000000000 "
+    fourth_column = "||||||||||| "
+    empty_column =  "           -"
+
+    columns = [first_column, second_column, third_column, fourth_column, empty_column]
+
+    bigger_name = ""
+
+    total_withdrawals = 0
+    for category in categories:
+        # Get total withdrawals
+        total_withdrawals += category.get_withdrawals()
+        # Get bigger name
+        if len(category.category) > len(bigger_name): bigger_name = category.category
+
+    # Pad columns
+    columns = [column + len(bigger_name) * " " for column in columns]
+
+    # Add items to columns
+    for category in categories:
+        percentage = 100 * category.get_withdrawals() / total_withdrawals
+        bar_height = int(percentage/10) + 1
+        columns.append(f"{bar_height * 'o':>11}-{category.category:{len(bigger_name)}}")
+        columns.append(columns[4])
+        columns.append(columns[4])
+
+    # Draw chart
+    chart = "Percentage spent by category\n"
+    for character in range(len(columns[0])):
+        for column in columns:
+            chart += column[character]
+        chart += "\n"
+
+    return chart[0:-1]  # The slicing removes last '\n' character to pass all tests. Not really necessary
